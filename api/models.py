0e30a4e79b04ad8ef9db5cf86d67770c1591d73e
Akash Gusain <akash.gusain.ext@nokia.com>
2021-03-10 15:32:35 +0530

    Added choices(dropdown) for Insurance(policy ) field

--- api/models.py
+++ api/models.py
@@ -3,6 +3,10 @@ from datetime import date
 from django.db import models
 from rest_framework.exceptions import ValidationError
 
+policy_choices = [('MI', 'Motor Insurance'), ('TI', 'Travel Insurance'), ('HI', 'Home Insurance'),
+                  ('PI', 'Property Insurance'), ('PA', 'Personal Accident'),
+                  ('WLI', 'Whole Life Insurance')]
+
 
 def no_future(value):
     today = date.today()
@@ -25,7 +29,7 @@ class Customer(models.Model):
 
 class Insurance(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    type = models.CharField(max_length=20, blank=False, null=False)
+    type = models.CharField(max_length=10, choices=policy_choices)
     premium = models.PositiveBigIntegerField(blank=False, null=False)
     cover = models.PositiveBigIntegerField(blank=False, null=False)
     created_at = models.DateTimeField(auto_now_add=True, auto_now=False, null=True)
